4a3a07dbab0aabb04e4c2ef02019b3d73ecbbea3
Philip Arena <philiplarena@gmail.com>
2019-04-06 14:18:41 -0400

    got song rank from html

--- get_lyrics.py
+++ get_lyrics.py
@@ -8,43 +8,42 @@ def strp_text(text):
     return text
 
 
+
 def get_hot_country_by_year(year):
-    print(year)
     url = 'https://www.billboard.com/charts/year-end/%s/hot-country-songs' % (year)
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
 
     song_ls = []
-    billboard_num = 1
-    for x in soup.findAll(class_='ye-chart-item__text'):
+    for x in soup.findAll(class_='ye-chart-item__primary-row'):
         song_artist = {}
-        for title in x.findAll(class_="ye-chart-item__title"):
-            song = strp_text(title.text)
 
-        for artist in x.findAll(class_="ye-chart-item__artist"):
-            artists = strp_text(artist.text)
+        for rank in x.findAll(class_='ye-chart-item__rank'):
+            rank = strp_text(rank.text)
+            song_artist['rank'] = rank
 
-        song_artist['song'] = song
-        song_artist['artist'] = artists
-        song_artist['chart_num'] = billboard_num
-        song_artist['year'] = year
+        for text in x.findAll(class_='ye-chart-item__text'):
+
+            for title in text.findAll(class_="ye-chart-item__title"):
+                song = strp_text(title.text)
+                song_artist['song'] = song
+
+            for artist in text.findAll(class_="ye-chart-item__artist"):
+                artists = strp_text(artist.text)
+                song_artist['artist'] = artists
+
         song_ls.append(song_artist)
-        billboard_num = billboard_num +1
     return song_ls
 
 
-info = get_hot_country_by_year(2012)
-
-for x in info:
-    print(x)
-
 
 all_yr = []
-year_list = [2010, 2011]
+year_list = [2010,2011,2012]
 for year in year_list:
+    print('getting year %s ' % str(year))
     dicts = {}
     info = get_hot_country_by_year(year)
     dicts['year'] = year
     dicts['info'] = info
     all_yr.append(dicts)
-    sleep(10)
+    sleep(3)
